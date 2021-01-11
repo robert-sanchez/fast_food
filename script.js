e7e9c6b773d7354040a50454d7ade20f0fb4525a
rjs <sancrob2@isu.edu>
2021-01-10 19:32:54 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const fastFoodRec = {
+    restaurant: ['McDonalds', 'Taco Bell', 'Burger King', 'Jack in the Box'],
+    qualities: ['great food', 'awesome service', 'low prices', 'healthy options'],
+    ordering: ['order now', 'get it tomorrow', 'pickup in an hour', 'get it delivered']
+}
+
+let foodMessege = [];
